31bbb759f44f49eaf27810fb421eaa836605d0a0
Otter-And-Bench <otterandbench@yahoo.com>
2025-11-25 18:46:38 -0600

    gameloop

--- scripts/person.gd
+++ scripts/person.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
+const PERSON = preload("uid://bf7js5xmur06k")
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 const RETURN = 0
+const THROWLEFT = 1
+const THROWRIGHT = 2
 
 var move_with_mouse = false
 var state = 0
@@ -12,6 +15,8 @@ var leftrw = (rw*2)/8
 var midrw = rw/2
 var rightrw = (rw*6)/8
 var startY
+
+# Q: Do we want to use this throw script or maybe we just use buttons? 
 
 func _ready():
 	startY = position.y
@@ -23,14 +28,34 @@ func _process(_delta):
 	else:
 		match state:
 			RETURN: #Return to the center of the screen
-				self.position = lerp(self.position, Vector2(midrw, startY), 0.25)
+				position = lerp(position, Vector2(midrw, startY), 0.2)
+			THROWLEFT: 
+				position = lerp(position, Vector2(-500.0, startY),0.05)
+				if position.x < -200.0:
+					_die()
+			THROWRIGHT: 
+				position = lerp(position, Vector2(rw+500.0, startY),0.05)
+				if position.x > rw+200.0:
+					_die()
+
+func _die():
+					print("dead")
+					var inst = PERSON.instantiate()
+					get_parent().add_child(inst)
+					inst.global_position = Vector2(midrw,startY)
+					inst.startY = startY
+					inst.z_index = -1
+					queue_free()
+	
 
 #Finalizes the players choice based on the cards x position
 func _on_selector_button_up() -> void:
 	if global_position.x > rightrw:
 		print("Right!")
+		state = THROWRIGHT
 	elif global_position.x < leftrw:
 		print("Left!")
+		state = THROWLEFT
 	else:
 		state = RETURN
 	move_with_mouse = false
